65b39abf24d98b9b2e291e88cda6cecf1a0a7fd5
Eric Stiens <estiens@server.fake>
2013-09-06 18:08:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EPLRails::Application.config.secret_key_base = '3ef1e1bdeaf1dbbd28768d410937dc539c0d247f97dbad1d516ff6c4baf2098a653f52364ed52f3fbb09ebc0dd384a3b93cf1f4e35a81100a2f565a865ed5cd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EPLRails::Application.config.session_store :cookie_store, key: '_EPL_rails_session'
